991358f34d33b7c7b81385bab0d584c2ae1143ad
Ziv.Perry <ziv.perry@gmail.com>
2020-12-10 12:30:01 +0200

    after build new version

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -1,9 +1,11 @@
+import { AxiosResponse } from 'axios';
 import { NameSpace, Vault } from './types';
+export declare const toData: (res: AxiosResponse) => any;
 export declare class Vaultier {
     readonly base: string;
     constructor(base?: string);
-    store(ns: NameSpace, vault: Vault): Promise<import("axios").AxiosResponse<boolean>>;
-    fetch(ns: NameSpace): Promise<import("axios").AxiosResponse<Record<string, string>>>;
-    flush(ns: NameSpace): Promise<import("axios").AxiosResponse<boolean>>;
+    store(ns: NameSpace, vault: Vault): Promise<boolean>;
+    fetch(ns: NameSpace): Promise<Vault>;
+    flush(ns: NameSpace): Promise<boolean>;
     isEmpty(ns: NameSpace): Promise<boolean>;
 }
